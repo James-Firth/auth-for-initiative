a9e3e3fb9139cc55e6b383a99cf0020d3ea1c33d
James Firth <james.firth@norimaconsulting.com>
2018-10-28 01:31:43 -0500

    Add dependencies to project

--- src/main.js
+++ src/main.js
@@ -1,10 +1,25 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
 import router from './router'
+import Sortable from 'sortablejs'
 import store from './store'
 import './registerServiceWorker'
 
 Vue.config.productionTip = false
+
+// Custom drag-n-drop sortable implementation from vuesortable github issues
+Vue.directive('sortable', {
+  inserted: function (el, binding) {
+    return new Sortable(el, binding.value || {})
+  }
+})
+
+// Using Font awesome
+Vue.use(Buefy, {
+  defaultIconPack: 'fas'
+})
 
 new Vue({
   router,
