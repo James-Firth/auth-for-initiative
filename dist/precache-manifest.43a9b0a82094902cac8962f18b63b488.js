35cb057debcc17dbd8b858bec2dc5dda91ca6ad1
James Firth <james.firth@norimaconsulting.com>
2018-11-01 01:45:37 -0500

    New build

---
+++ dist/precache-manifest.43a9b0a82094902cac8962f18b63b488.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "f04b719d8d17010c7395",
+    "url": "/js/chunk-2d216b64-legacy.ded1c40f.js"
+  },
+  {
+    "revision": "a81b4cc1702788d379baffbc0fd04804",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e52a711bbec5f999a6c",
+    "url": "/js/chunk-vendors-legacy.5e1a08b3.js"
+  },
+  {
+    "revision": "886c8c99d1d316163a81",
+    "url": "/js/chunk-169d8cf3-legacy.08a38975.js"
+  },
+  {
+    "revision": "c9e8dc5bb9d76d11df9f",
+    "url": "/js/chunk-2d0cf8b6-legacy.3302bbba.js"
+  },
+  {
+    "revision": "189d4442b7399cc1f0ff",
+    "url": "/js/app-legacy.cb2c01a7.js"
+  },
+  {
+    "revision": "fc37480edd76ee280de3",
+    "url": "/js/chunk-a8faff24-legacy.e95ae52c.js"
+  },
+  {
+    "revision": "8e52a711bbec5f999a6c",
+    "url": "/css/chunk-vendors.b8c57645.css"
+  },
+  {
+    "revision": "fc37480edd76ee280de3",
+    "url": "/css/chunk-a8faff24.ebf4429f.css"
+  },
+  {
+    "revision": "886c8c99d1d316163a81",
+    "url": "/css/chunk-169d8cf3.8901d7cd.css"
+  },
+  {
+    "revision": "189d4442b7399cc1f0ff",
+    "url": "/css/app.bced0c06.css"
+  }
+];
---
+++ dist/precache-manifest.a70b255c23f730108c16c7135ddce125.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1b081df5fe6034b9e0729a10890c507",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  },
+  {
+    "revision": "bd074136ab7843dc27b7edbd482f8b82",
+    "url": "/images/icons/icon-128x128.png"
+  },
+  {
+    "revision": "886c8c99d1d316163a81",
+    "url": "/css/chunk-169d8cf3.8901d7cd.css"
+  },
+  {
+    "revision": "c9e8dc5bb9d76d11df9f",
+    "url": "/js/chunk-2d0cf8b6.3302bbba.js"
+  },
+  {
+    "revision": "f04b719d8d17010c7395",
+    "url": "/js/chunk-2d216b64.ded1c40f.js"
+  },
+  {
+    "revision": "fc37480edd76ee280de3",
+    "url": "/css/chunk-a8faff24.ebf4429f.css"
+  },
+  {
+    "revision": "fc37480edd76ee280de3",
+    "url": "/js/chunk-a8faff24.e95ae52c.js"
+  },
+  {
+    "revision": "8e52a711bbec5f999a6c",
+    "url": "/css/chunk-vendors.b8c57645.css"
+  },
+  {
+    "revision": "8e52a711bbec5f999a6c",
+    "url": "/js/chunk-vendors.5e1a08b3.js"
+  },
+  {
+    "revision": "f0b66e61fb32c44f4e78",
+    "url": "/js/app.adfa7200.js"
+  },
+  {
+    "revision": "886c8c99d1d316163a81",
+    "url": "/js/chunk-169d8cf3.08a38975.js"
+  },
+  {
+    "revision": "ccef52ff73c9be690eaae5dd4e441c92",
+    "url": "/images/icons/icon-144x144.png"
+  },
+  {
+    "revision": "09ab66320bafe6979ec01a5221732cab",
+    "url": "/images/icons/icon-152x152.png"
+  },
+  {
+    "revision": "6806c71c49dadb49131d30be2d4775cb",
+    "url": "/images/icons/icon-192x192.png"
+  },
+  {
+    "revision": "3610cd43ce84462b39f96e510ac7d49b",
+    "url": "/images/icons/icon-384x384.png"
+  },
+  {
+    "revision": "2cbb36a01a9c0741af4d5b76028b4132",
+    "url": "/images/icons/icon-512x512.png"
+  },
+  {
+    "revision": "cf8b80dcba6af78bb0132c484fa80dc4",
+    "url": "/images/icons/icon-72x72.png"
+  },
+  {
+    "revision": "66a6f8576acc03cf0b968d00d71433f0",
+    "url": "/images/icons/icon-96x96.png"
+  },
+  {
+    "revision": "7ddda2ff8afba2cf14dcedf6ce504c09",
+    "url": "/img/kanka_logo.png"
+  },
+  {
+    "revision": "f0b66e61fb32c44f4e78",
+    "url": "/css/app.bced0c06.css"
+  }
+];
